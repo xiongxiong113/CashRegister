2ee836d9b88ddabc57c8b911084a1fff17782bee
Jiahui Xiong <jxiong5@slb.com>
2022-10-26 16:07:46 +0800

    feat: use moq for test(question2)

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,6 +1,7 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
+	using Moq;
 	using Xunit;
 
 	public class CashRegisterTest
@@ -18,5 +19,18 @@ namespace CashRegisterTest
 			//verify that cashRegister.process will trigger print
 			Assert.True(printer.HasPrinted);
 		}
-	}
+
+		[Fact]
+		public void Should_print_called_When_Process_Given_spy_printer_create()
+		{
+			//given
+			var printer = new Mock<Printer>();
+            var cashRegister = new CashRegister(printer.Object);
+            var purchase = new Purchase();
+            //when
+            cashRegister.Process(purchase);
+			//then
+			printer.Verify(_ => _.Print(It.IsAny<string>()));
+        }
+    }
 }
